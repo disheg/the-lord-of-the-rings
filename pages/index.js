ce282cf22600c588dc2c283d4b154ed48cd6d544
disheg <disheg@gmail.com>
2020-12-14 17:25:53 +0100

    delete Headers

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,7 @@
 import { MainLayout } from '../components/MainLayout';
-import Header from './Header';
-
 export default function Home() {
   return (
     <MainLayout>
-      <Header />
-      <content></content>
     </MainLayout>
   )
 }
